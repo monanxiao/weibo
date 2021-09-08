15d6d7e18da60346b75bc6050199c9e9400ccf41
monanxiao <monanxiao@qq.com>
2021-09-08 14:11:02 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,12 +24,24 @@ class UsersController extends Controller
 
 
         //表单字段验证规则
-        $this->validate($request,[
+        $this->validate($request, [
             'name' => 'required|unique:users|max:50',
-            'email' => 'required|emai|unique:users|max:255',
+            'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
 
-        return ;
+        //保存用户数据
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        //闪存消息 提醒注册成功
+        session()->flash('success', '欢迎，您在这里将会开启一段新的启程~');
+
+        //注册完成后跳转到用户首页
+        return redirect()->route('users.show', [$user]);
+
     }
 }
