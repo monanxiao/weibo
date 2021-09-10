3f68d54acf7cebee1db8f513945da31bcdbd7f5e
monanxiao <monanxiao@qq.com>
2021-09-10 12:49:14 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,16 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Auth;
+
 
 class StaticPagesController extends Controller
 {
     //首页
     public function home()
     {
-
         // return '首页';
+        // return view('static_pages/home');
 
-        return view('static_pages/home');
+        $feed_items = [];// 声明空数组
+
+        // 验证用户是否登录
+        if(Auth::check()) {
+
+            // 用户登录后赋值当前用户发布的微博列表 并分页显示
+            $feed_items = Auth::user()->feed()->paginate(10);
+            // $feed_items = Auth::user()->feed()->paginate(30);
+        }
+
+        return view('static_pages/home', compact('feed_items'));
+
     }
 
     //帮助页
--- app/Models/User.php
+++ app/Models/User.php
@@ -70,4 +70,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::class);
     }
+
+    // 倒序显示微博数据
+    public function feed()
+    {
+        return $this->statuses()
+                    ->orderBy('created_at','desc');
+    }
 }
